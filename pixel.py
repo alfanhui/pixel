cab8adc96d06e28ca913722a1e0e5d463ce68967
Stuart Huddy <s.w.m.huddy@dundee.ac.uk>
2016-07-12 20:13:10 +0200

    Starting the conversion from Java to Python

---
+++ pixel.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class pixel():
+    __slots__ = {'outData', 'inData', 'dimension'}
+
+    def __init__():
+        dimension = 400
+        outData = np.zeros(dimension, dimension)
+
+
+    def start():
+        plt.figure(1)
+
+
+    def randomise():
+        outData = np.random.randint(2, size=(dimension, dimension))
+
+
+
+
+if __name__ == '__main__':
+    pixel = pixel()
+    pixel.start()
+
+
